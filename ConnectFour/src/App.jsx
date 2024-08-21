14247a5a37b01329ea32890a3283f4cf5f08068a
Jacob Stone <jumboshrimp9554@gmail.com>
2024-08-21 15:00:50 -0400

    Gameboard: Created the board itself. 7 rows and 6 columns

--- ConnectFour/src/App.jsx
+++ ConnectFour/src/App.jsx
@@ -1,5 +1,82 @@
 import { useState } from 'react';
 
-export default function Circle() {
+/*
+    Circle is the component that makes up the game board.
+*/
+function Circle() {
     return <button className="circle"></button>
+}
+
+/*
+    Board is what players play on. Standard Connect4 Board is 6 columns by 7 rows (6x7)
+    NOTE: This isn't very elegant. Find a better solution?
+*/
+export default function Board() {
+    return (
+        <>
+            <div className="board-row">
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+            </div>
+            <div className="board-row">
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+            </div>
+            <div className="board-row">
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+            </div>
+            <div className="board-row">
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+            </div>
+            <div className="board-row">
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+            </div>
+            <div className="board-row">
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+            </div>
+            <div className="board-row">
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+                <Circle />
+            </div>
+        </>
+    );
 }
