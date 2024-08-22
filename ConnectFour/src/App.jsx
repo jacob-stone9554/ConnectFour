31a285f3181a09f0a93ba811ad83c13630eed2bd
Jacob Stone <jumboshrimp9554@gmail.com>
2024-08-22 12:24:53 -0400

    GameBoard: Developed a more elegant solution to generate the circles

--- ConnectFour/src/App.jsx
+++ ConnectFour/src/App.jsx
@@ -9,74 +9,29 @@ function Circle() {
 
 /*
     Board is what players play on. Standard Connect4 Board is 6 columns by 7 rows (6x7)
-    NOTE: This isn't very elegant. Find a better solution?
 */
 export default function Board() {
+    const [circles, setCircles] = useState(Array(42).fill(null)); // the elements of the array represent the circles on the board. All are null until it is clicked (which is then assigned a value)
+
+    // helper function. takes an index, returns a Circle with key and index prop.
+    const renderCircle = (index) => {
+        return <Circle key={index} index={index} />;
+    };
+
+    // handleClick determines what happens to a circle when it is clicked. right now, simply change the color.
+
+    /*
+        This looks a little complicated (to me at least).
+        Return a div that will contain the game board.
+        Array(6).fill creates an array 
+    */
     return (
-        <>
-            <div className="board-row">
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-            </div>
-            <div className="board-row">
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-            </div>
-            <div className="board-row">
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-            </div>
-            <div className="board-row">
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-            </div>
-            <div className="board-row">
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-            </div>
-            <div className="board-row">
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-            </div>
-            <div className="board-row">
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-                <Circle />
-            </div>
-        </>
+        <div className="board">
+            {Array(6).fill(null).map((_, rowIndex) => (
+                <div key={rowIndex} className="board-row">
+                    {Array(7).fill(null).map((_, colIndex) => renderCircle(rowIndex * 7 + colIndex))}
+                </div>
+            ))}
+        </div>
     );
 }
